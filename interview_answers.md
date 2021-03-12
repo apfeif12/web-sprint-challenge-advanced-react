c00f9f288306a084cb6e3af437412664a6920903
alex-pfeifer <apfeif.12@gmail.com>
2021-03-12 12:11:35 -0800

    mvp

--- interview_answers.md
+++ interview_answers.md
@@ -2,9 +2,12 @@
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. These will not be counted as a part of your sprint score but will be helpful for preparing you for your endorsement interview, and enhancing overall understanding.
 
 1. What are the main differences between a stateful and a functional component?
-
+stateful components keep track of dynamic data while function components get their data through props
 2. When does a componentWillMount function be called? What about a componentWillUpdate?
-
+componentWillMount runs after the first render, and componentWillUpdate runs after the render finishes loading
 3. Define stateful logic.
-
+any code that uses state or the behavior caused by the use of one or more hooks
 4. What are the three step of creating a successful test? What is done in each phase?
+arrange: renders stuff to the dom
+act: queries the dom for the selected element
+assert: check that the queried element exists in the dom 
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,47 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+  render(<CheckoutForm />);
+  const fNameInput = screen.getByLabelText(/First Name/i);
+  const lNameInput = screen.getByLabelText(/Last Name/i);
+  const addressInput = screen.getByLabelText(/Address/i);
+  const cityInput = screen.getByLabelText(/City/i);
+  const stateInput = screen.getByLabelText(/State/i);
+  const zipInput = screen.getByLabelText(/Zip/i);
+
+  userEvent.type(fNameInput, "Alex");
+  userEvent.type(lNameInput, "Pfeifer");
+  userEvent.type(addressInput, "123 street");
+  userEvent.type(cityInput, "victoria");
+  userEvent.type(stateInput, "bc");
+  userEvent.type(zipInput, "12345");
+
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const newFNameInput = await screen.findByText(/alex/i);
+  const newLNameInput = await screen.findByText(/Pfeifer/i);
+  const newAddressInput = await screen.findByText(/123 street/i);
+  const newCityInput = await screen.findByText(/victoria/i);
+  const newStateInput = await screen.findByText(/bc/i);
+  const newZipInput = await screen.findByText(/12345/i);
+
+  expect(newFNameInput).toBeInTheDocument();
+  expect(newLNameInput).toBeInTheDocument();
+  expect(newAddressInput).toBeInTheDocument();
+  expect(newCityInput).toBeInTheDocument();
+  expect(newStateInput).toBeInTheDocument();
+  expect(newZipInput).toBeInTheDocument();
+
+  const success = await screen.findByTestId("successMessage");
+  expect(success).toBeInTheDocument();
+});
